02406938b0b871dc8f4fbb6d99c0cd21701aa8f9
laurensruiz <laurenruiz97@yahoo.com>
2022-05-24 13:05:51 -0400

    made find by and find by id in model

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -38,6 +38,10 @@ function findBy(filter) {
       }
     ]
    */
+  return db('users')
+    .join('roles', 'users.role_id', 'roles.role_id')
+    .select('user_id', 'username', 'role_name', 'password')
+    .where(filter)
 }
 
 function findById(user_id) {
@@ -51,6 +55,10 @@ function findById(user_id) {
       "role_name": "instructor"
     }
    */
+    return db('users')
+    .join('roles', 'users.role_id', 'roles.role_id')
+    .select('user_id', 'username', 'role_name')
+    .where('users.user_id', user_id)
 }
 
 /**
